0ffd362a0291cfe94f9b16282cd30a0bbfeb0838
yanxin <yanxin@zmexing.com>
2026-01-29 21:41:56 +0800

    111

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Django-Admin
 Django Admin管理后台
+
+```text
+这是一个基于Django框架的管理后台项目，旨在提供一个易于使用和扩展的管理界面，方便用户对数据进行增删改查操作。
+```
+
+```shell
+# 导入依赖
+pip freeze > requirements.txt
+```
+
+```shell
+# 运行项目
+python manage.py runserver
+```
--- admin/urls.py
+++ admin/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import hello
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello, name="hello"),
 ]
