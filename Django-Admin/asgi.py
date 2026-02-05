b040a3097ab6e40c86345edccbff8b20b4b9cef9
yanxin666 <18515570803@163.com>
2026-02-05 11:44:42 +0800

    format

--- Django-Admin/asgi.py
+++ Django-Admin/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django-Admin.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django-Admin.settings")
 
 application = get_asgi_application()
--- Django-Admin/settings.py
+++ Django-Admin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!%r=w15c5oi5)sf7hvgg)ts968ks+v-c8#kdanzf$=u+)gl)a-'
+SECRET_KEY = "django-insecure-!%r=w15c5oi5)sf7hvgg)ts968ks+v-c8#kdanzf$=u+)gl)a-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,66 +31,73 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'customer',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "customer",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'Django-Admin.urls'
+ROOT_URLCONF = "Django-Admin.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'Django-Admin.wsgi.application'
+WSGI_APPLICATION = "Django-Admin.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    "default": {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
-        'NAME': 'djanggo',        # 你创建的数据库名
-        'USER': 'root',             # 你的 MySQL 用户名（如 root）
-        'PASSWORD': 'qOnFC?wvbF*&HvT',         # 你的 MySQL 密码
-        'HOST': '49.232.253.114',                 # 数据库主机，本地一般为 127.0.0.1 或 localhost
-        'PORT': '3306',                      # 端口，默认为 3306
-        'OPTIONS': {
-            'charset': 'utf8mb4',     # 字符集
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # 严格模式
+        "ENGINE": "django.db.backends.mysql",  # 数据库引擎
+        "NAME": "djanggo",  # 你创建的数据库名
+        "USER": "root",  # 你的 MySQL 用户名（如 root）
+        "PASSWORD": "qOnFC?wvbF*&HvT",  # 你的 MySQL 密码
+        "HOST": "49.232.253.114",  # 数据库主机，本地一般为 127.0.0.1 或 localhost
+        "PORT": "3306",  # 端口，默认为 3306
+        "OPTIONS": {
+            "charset": "utf8mb4",  # 字符集
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",  # 严格模式
             # 如果需要更低的连接时间，可以添加以下选项
-            'connect_timeout': 10,
+            "connect_timeout": 10,
         },
         # 连接池相关配置（可选）
-        'CONN_MAX_AGE': 60,  # 连接最长保持时间（秒）
+        "CONN_MAX_AGE": 60,  # 连接最长保持时间（秒）
+        "CONN_HEALTH_CHECKS": True,  # 连接健康检查（Django 3.1+）
+        # 连接池
+        "POOL_OPTIONS": {
+            "POOL_SIZE": 10,
+            "MAX_OVERFLOW": 20,
+            "RECYCLE": 3600,
+        },
     }
 }
 
@@ -100,16 +107,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -117,9 +124,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh-hans'
+LANGUAGE_CODE = "zh-hans"
 
-TIME_ZONE = 'Asia/Shanghai'
+TIME_ZONE = "Asia/Shanghai"
 
 USE_I18N = True
 
@@ -131,6 +138,6 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240  # 如果遇到字段数量限制
